fa197b1fb770458b27852b99fb34a94e2ffbcec9
Tony <jalvarez@grupomas.software>
2026-04-22 08:02:56 -0300

    Fix mobile shell startup crash

--- Handheld/AppShell.xaml.cs
+++ Handheld/AppShell.xaml.cs
@@ -1,16 +1,57 @@
-﻿using Handheld.Views;
+using Handheld.Views;
+using Microsoft.Extensions.DependencyInjection;
 
 namespace Handheld;
 
 public partial class AppShell : Shell
 {
-    public AppShell()
+    private readonly IServiceProvider _services;
+
+    public AppShell(IServiceProvider services)
     {
         InitializeComponent();
-        Routing.RegisterRoute("requestdetail", typeof(MyRequestDetails));
-        Routing.RegisterRoute("mynewrequest", typeof(ServiceRequestPage));
-        Routing.RegisterRoute("register", typeof(RegisterPage));
-        Routing.RegisterRoute("change-password", typeof(ChangePasswordPage));
-        Routing.RegisterRoute("about", typeof(AboutPage));
+        _services = services;
+
+        Items.Add(CreateShellContent<LoginPage>("login"));
+        Items.Add(CreateShellContent<HomePage>("home"));
+        Items.Add(CreateShellContent<MyRequestsPage>("myrequests"));
+        Items.Add(CreateShellContent<ServicesPage>("services"));
+
+        Routing.RegisterRoute("requestdetail", new ServiceProviderRouteFactory<MyRequestDetails>(_services));
+        Routing.RegisterRoute("mynewrequest", new ServiceProviderRouteFactory<ServiceRequestPage>(_services));
+        Routing.RegisterRoute("register", new ServiceProviderRouteFactory<RegisterPage>(_services));
+        Routing.RegisterRoute("change-password", new ServiceProviderRouteFactory<ChangePasswordPage>(_services));
+        Routing.RegisterRoute("about", new ServiceProviderRouteFactory<AboutPage>(_services));
+    }
+
+    private ShellContent CreateShellContent<TPage>(string route)
+        where TPage : Page
+    {
+        return new ShellContent
+        {
+            Route = route,
+            ContentTemplate = new DataTemplate(() => _services.GetRequiredService<TPage>())
+        };
+    }
+
+    private sealed class ServiceProviderRouteFactory<TPage> : RouteFactory
+        where TPage : Element
+    {
+        private readonly IServiceProvider _services;
+
+        public ServiceProviderRouteFactory(IServiceProvider services)
+        {
+            _services = services;
+        }
+
+        public override Element GetOrCreate()
+        {
+            return _services.GetRequiredService<TPage>();
+        }
+
+        public override Element GetOrCreate(IServiceProvider services)
+        {
+            return services.GetRequiredService<TPage>();
+        }
     }
 }
